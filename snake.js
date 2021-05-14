0ee4c8c3c5f79aa03e76a6f1e1f9507f4d86c792
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-14 15:17:58 -0500

    Animating whole game on a loop and checking to see if snake ate food.

--- snake.js
+++ snake.js
@@ -59,3 +59,35 @@ function keydown(e){
         }
     }
 }
+
+setInterval(() => {
+    requestAnimationFrame(gameLoop);
+}, 1000 /FR);
+
+function gameLoop(){
+    ctx.fillStyle = Bg_clr;
+    ctx.fillReact(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = Snake_clr;
+    for (let cell of snake) {
+        ctx.fillRect(cell.x*S, cell.y*S, S,S);
+    }
+    
+    ctx.fillStyle = Food_clr;
+    ctx.fillReact(food.x*S, food.y*S,S,S);
+
+    pos.x += vel.x;
+    pos.y += vel.y;
+
+    if (pos.x < 0 || pos.x > T || pos.y < 0 || pos.y > T){
+      init();  
+    }
+
+    if (food.x === pos.x && food.y === pos.y){  /*Checking to see if the snake ate the food */
+        snake.push({...pos});
+        pos.x += vel.x;
+        pos.y += vel.y;
+        randomFood();
+    }
+
+}
