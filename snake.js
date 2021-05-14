209e87a6a303b0d609a699b89111671c8fcd3b20
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-14 14:18:05 -0500

    Creating Frame Rate, Screen Size and Tile Size.

--- snake.js
+++ snake.js
@@ -2,3 +2,11 @@ const Bg_Clr = '#F8DF77';
 const Snake_Clr = '#2ECFCA';
 const Food_clr = '#FF4E62';
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = canvas.height = 400;
+
+const FR = 10; //Frame Rate
+const S = 20; //Screen size
+const T = canvas.width / S; //Tile size Canvas width divided by Screen size
