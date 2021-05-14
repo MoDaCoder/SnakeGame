ba7c4f318948cc68548f4ddbd76f2902a7396739
gmartinez2014 <gmartinez2014@appddictionstudio.com>
2021-05-14 14:25:50 -0500

    Creating initial snake.

--- snake.js
+++ snake.js
@@ -10,3 +10,17 @@ canvas.width = canvas.height = 400;
 const FR = 10; //Frame Rate
 const S = 20; //Screen Size
 const T = canvas.width / S; //Tile Size Canvas Width divided by Screen Size
+
+let pos /*position*/, vel /*velocity*/, food, snake;
+
+function init(){
+    pos = {x: 10, y: 10};
+    vel = {x: 0, y: 0};
+
+    snake = [
+        {x: 8, y: 10},
+        {x: 9, y: 10},
+        {x: 10, y: 10},
+    ]
+    
+}
